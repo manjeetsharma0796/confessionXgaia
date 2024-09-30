f48b4c29822301eca3a60e6b0bed3aa87ab6335e
manjeet0796 <manjeetsharma0796@gmail.com>
2024-09-30 18:19:11 +0530

    backend update

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -57,7 +57,7 @@ const Home: FC = () => {
             setInputText("");
 
             try {
-                const response = await fetch('http://127.0.0.1:5000/chat_gen', {
+                const response = await fetch('http://127.0.0.1:8000/chat_gen', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -117,6 +117,11 @@ const Home: FC = () => {
                             type="text"
                             value={inputText}
                             onChange={(e) => setInputText(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    handleSendMessage();
+                                }
+                            }}
                             className="flex-grow p-3 border rounded bg-black text-green-300 placeholder-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 glow-input"
                             placeholder="Type your message..."
                             style={{
